14a656b9af1821054f3151de43fd9509375d6317
ahmed anis <keyloggerpy2022@gmail.com>
2023-03-14 18:31:26 +0300

    final udate

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let [seconds,minuts,hours] = [0,0,0],
     displayTime = document.getElementById("display"),
+    message = document.getElementById("message"),
     timer = null;
 
 function stopWatch(){
@@ -21,13 +22,16 @@ function start(){
     if (timer != null){
         clearInterval(timer);
     }
+    message.innerHTML = "timer starts"
     timer = setInterval(stopWatch,1000);
 }
 function pausTimer(){
     clearInterval(timer)
+    message.innerHTML = "timer paused"
 }
 function resetTimer(){
     clearInterval(timer);
     [seconds,minuts,hours] = [0,0,0];
     displayTime.innerHTML = "00:00:00";
+    message.innerHTML = "timer reseted"
 }
